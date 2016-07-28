d604f8937c6bd8f23c539eb223105c38c1323124
elifoster <elifosterwy@gmail.com>
2016-07-27 17:05:43 -0700

    :birthday: Support MC 1.10.2. Close #3

--- src/main/java/santa/seedcopy/SeedCopyMod.java
+++ src/main/java/santa/seedcopy/SeedCopyMod.java
@@ -4,11 +4,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
-@Mod(name = SeedCopyMod.NAME, modid = SeedCopyMod.MODID, version = SeedCopyMod.VERSION, acceptedMinecraftVersions = "[1.9,1.10]")
+@Mod(name = SeedCopyMod.NAME, modid = SeedCopyMod.MODID, version = SeedCopyMod.VERSION, acceptedMinecraftVersions = "[1.9,1.10.2]")
 public class SeedCopyMod {
     public static final String NAME = "SeedCopy";
     public static final String MODID = "seedcopy";
-    public static final String VERSION = "3.0.1";
+    public static final String VERSION = "3.0.2";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
