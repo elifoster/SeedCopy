def39afadda83bff3932f4cce512422490f81d07
elifoster <elifosterwy@gmail.com>
2017-05-28 10:48:33 -0700

    :birthday: Bump version to 3.0.3

--- src/main/java/santa/seedcopy/SeedCopyMod.java
+++ src/main/java/santa/seedcopy/SeedCopyMod.java
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class SeedCopyMod {
     public static final String NAME = "SeedCopy";
     public static final String MODID = "seedcopy";
-    public static final String VERSION = "3.0.2";
+    public static final String VERSION = "3.0.3";
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
